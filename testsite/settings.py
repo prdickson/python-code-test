febb2ae4e8ea18a3a1915e5738db3a870702a093
paul <prdickson@gmail.com>
2018-05-19 11:52:45 +0100

    Added django rest frame work and dependencies.

--- testsite/settings.py
+++ testsite/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'django_extensions',
     'shiptrader',
 ]
 
@@ -127,3 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
