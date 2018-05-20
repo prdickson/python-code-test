f954977a928f24b18e8331762fd8e86cf616a9f5
paul <prdickson@gmail.com>
2018-05-20 17:31:42 +0100

    Removed unnecessary name constraint.

--- shiptrader/migrations/0002_auto_20180519_1004.py
+++ shiptrader/migrations/0002_auto_20180519_1004.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='starship',
             name='name',
-            field=models.CharField(max_length=255, unique=True),
+            field=models.CharField(max_length=255),
             preserve_default=False,
         ),
     ]
--- shiptrader/models.py
+++ shiptrader/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Starship(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
 
     starship_class = models.CharField(max_length=255)
